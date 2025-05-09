0f6ca6abf06ee5bb0253f67936fa63ad85e6e4f6
Malachie04 <malakimalachie@gmail.com>
2025-05-09 11:57:19 +0200

    Third commit message

--- scripts/main.js
+++ scripts/main.js
@@ -1,64 +1,6 @@
 
-
-// const body = document.querySelector('body');
-// const liste= document.querySelector('.mylist');
-// const bouton= document.querySelector('.btn');
-// const cont = document.querySelector('p');
-
-// let userEl=document.querySelector('.userSaisi');
-
-// let mylist = [];
-
-// bouton.addEventListener('click',(event)=>{
-//     event.preventDefault();
-//     let userSaisie = document.querySelector('.userSaisi').value;
-//     if(!userSaisie){
-//         document.querySelector('.userSaisi').focus();
-//         return;
-//     }else if(mylist.some(item => item.toUpperCase() === userSaisie.toUpperCase())){
-//         document.querySelector('.userSaisi').select();
-//         return;
-//     }
-
-
-//     mylist.push(userSaisie);
-//     Afficher();
-//     cont.innerHTML =`Element(s) ajouté(s) :${mylist.length}`;
-//     userEl.value='';
-//     userEl.focus();
-// });
-
-// function remove(event) {
-//     let value = event.target.getAttribute('data-value');
-//     console.log(value);
-//     mylist.splice(value, 1);
-//     updateList();
-//     Afficher();
-//     userEl.focus();
-// }
-
-// function updateList() {
-//     cont.innerHTML =`Element(s) ajouté(s) :${mylist.length}`;
-// }
-// function Afficher(){
-//     liste.innerHTML ='';
-//     mylist.forEach((element)=>{
-//         liste.innerHTML += `
-//         <li>${element} <button type="submit" class="supprimer" data-value=${mylist.indexOf(element)}><i class="fa-solid fa-trash"></i></button></li>`;
-//     });
-    
-//     let btnsupprimer= document.querySelectorAll('.supprimer');
-//     btnsupprimer.forEach((button)=>{
-//        button.addEventListener('click',(event)=>{
-//            remove(event);
-//        });
-//    });
-
-// }
-
 let myCourseList = [];
 
-// console.log(myCourseList);
 
 //Selection des elements
 const description= document.querySelector('.description');
@@ -95,6 +37,10 @@ ajouter.addEventListener('click',(event)=>{
     category.value = '';
     console.log(myCourseList);
     displayCourse();
+
+    document.querySelector('.delete').addEventListener('click', (event) => {
+        removeCourse(event);
+    });
 });
 
 
@@ -103,7 +49,6 @@ const addCourse=(descrition,montant,categorie)=>{
 }
 
 const displayCourse=()=>{
-
     if(afficher.hasChildNodes()){
         afficher.innerHTML='';
     }
@@ -111,11 +56,24 @@ const displayCourse=()=>{
     total=0;
     myCourseList.forEach((element)=>{  
         const li=document.createElement('li');
-        li.innerHTML=`<span>${element[0]}</span><span>-</span><span>${element[1]}</span><span>-</span><span> ${element[2]}</span><a href="#" class="delete" date-value=${myCourseList.indexOf(element)}>🗑️</a>`;
+        li.attributes.setNamedItem(document.createAttribute('value'));
+        li.setAttribute('value', myCourseList.indexOf(element));
+
+        li.innerHTML=`<span>${element[0]}</span><span>-</span><span>${element[1]}</span><span>-</span><span> ${element[2]}</span><button href="#" class="delete">🗑️</button>`;
+
+        li.querySelector('.delete').addEventListener('click', (event) => {
+            const index = myCourseList.indexOf(element);
+            removeCourse(index); 
+        });
+
         total+= parseInt(element[1]);
         ul.appendChild(li);
     });
     afficher.appendChild(ul);
     totalCourse.innerHTML=`Total : ${total} €`;
-    // container.appendChild(divAfficher);
+}
+const removeCourse=(index)=>{
+    myCourseList.splice(index, 1);
+    displayCourse();
+    displayCourse();
 }
